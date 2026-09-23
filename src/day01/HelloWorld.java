4bfabf146bc2235f53bb063adb37fc1e197d4615
aiperiaibekova <103370683+aiperiaibekova@users.noreply.github.com>
2026-09-22 22:54:01 -0500

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello Cydeo");
-        System.out.println("Hello Programmers");
+        System.out.println("Hello Programmer");
     }
 }
